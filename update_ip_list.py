cc17a74d71639822c9ed0ef14d7303d3e86196cd
Muhammad <31498696+moli1369@users.noreply.github.com>
2025-03-06 10:22:08 +0330

    Delete update_ip_list.py

--- update_ip_list.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "https://lite.ip2location.com/iran-(islamic-republic-of)-ip-address-ranges?lang=en_US"  # آدرس منبع IP
-response = requests.get(url)
-with open("ip-list.txt", "w") as file:
-    file.write(response.text)
