634bbc4a6fe9858f6954b8d9ef244f6f89e6140e
Muhammad <31498696+moli1369@users.noreply.github.com>
2025-03-06 10:12:02 +0330

    Update update_ip_list.py

--- .github/workflows/update_ip_list.py
+++ .github/workflows/update_ip_list.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://lite.ip2location.com/iran-(islamic-republic-of)-ip-address-ranges?lang=en_US'  # آدرس منبع IP
+url = 'https://raw.githubusercontent.com/moli1369/Ip-List-mikrotik/refs/heads/main/Iran-mikrotik'  # آدرس منبع IP
 response = requests.get(url)
 with open("ip-list.txt", "w") as file:
     file.write(response.text)
