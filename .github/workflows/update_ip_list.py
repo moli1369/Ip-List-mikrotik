1aad518ec663a5619db963ef8dc306e91e753cd1
Muhammad <31498696+moli1369@users.noreply.github.com>
2025-03-06 10:21:59 +0330

    Delete .github/workflows directory

--- .github/workflows/update_ip_list.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'https://raw.githubusercontent.com/moli1369/Ip-List-mikrotik/refs/heads/main/Iran-mikrotik'  # آدرس منبع IP
-response = requests.get(url)
-with open("ip-list.txt", "w") as file:
-    file.write(response.text)
--- .github/workflows/update_ip_list.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -o ip-list.txt https://lite.ip2location.com/iran-(islamic-republic-of)-ip-address-ranges?lang=en_US
